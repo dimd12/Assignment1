63b74320e6b10c88652e8c26bc6a32af757e35d5
Adrian Manoil <adi.manoil@outlook.com>
2026-09-03 10:58:40 +0200

    Added the project file

---
+++ src/main/java/com/mycompany/calendaristicdate/CalendaristicDate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calendaristicdate;
+
+/**
+ *
+ * @author adrian
+ */
+public class CalendaristicDate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
